db4df47b3f24ac7f9d7fb2adc447132aba41a9d2
Luke Sikina <lucas@ds.dfci.harvard.edu>
2021-02-25 11:30:51 -0500

    asdsa

--- web/src/main/java/org/cbioportal/web/ImportController.java
+++ web/src/main/java/org/cbioportal/web/ImportController.java
@@ -1,7 +1,14 @@
 package org.cbioportal.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.cbioportal.model.ImportLog;
 import org.cbioportal.model.ImportLogType;
 import org.cbioportal.model.ImportStudy;
@@ -23,6 +30,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -33,156 +43,137 @@ import java.util.concurrent.Semaphore;
 @Validated
 @Api(tags = "Import", description = " ")
 public class ImportController {
+    private final CloseableHttpClient httpClient = HttpClients.createDefault();
+    
     @Autowired
     ImportService importService;
-    
-    @Autowired
-    SecurityRepository securityRepository;
 
-    @Value("${db.password}")
-    String dbPassword;
-    
-    @Value("${db.host}")
-    String dbHost;
-    
-    @Value("${importer.log}")
-    String logDir;
-    
-    @Value("${host}")
-    String host;
-
-    @Value("${importer.bin}")
-    String importer;
-    
-    private static final ConcurrentHashMap<String, Semaphore> importLocks = new ConcurrentHashMap<>();
-    private static final ConcurrentHashMap<String, Semaphore> validationLocks = new ConcurrentHashMap<>();
-
-    @PreAuthorize("hasPermission(#studyId, 'CancerStudyId', 'read')")
     @RequestMapping(value = "/logs/{logType}/{studyId}/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Get the specified log file")
     public ResponseEntity<ImportLog> getLog(
-        @PathVariable("logType") String logType,
+        @PathVariable("logType") String logType, // this can be deleted now. id is unique
         @PathVariable("studyId") String studyId,
-        @PathVariable("id") String id
+        @PathVariable("id") String id,
+        Authentication authentication
     ) {
-        ImportLog log = importService.getLog(logType, studyId, id);
-        return new ResponseEntity<>(log, log == null ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+        String userId = getUserId(authentication);
+        HttpGet request = new HttpGet("importer:8080/logs/" + studyId + "/" + id + "/" + userId);
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            HttpStatus status = HttpStatus.resolve(response.getStatusLine().getStatusCode());
+            ObjectMapper mapper = new ObjectMapper();
+            ImportLog importLog = mapper.readValue(EntityUtils.toString(response.getEntity()), ImportLog.class);
+            return new ResponseEntity<>(importLog, status == null ? HttpStatus.INTERNAL_SERVER_ERROR : status);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PreAuthorize("hasPermission(#studyId, 'CancerStudyId', 'read')")
     @RequestMapping(value = "/logs/{logType}/{studyId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Get the specified log file")
     public ResponseEntity<List<ImportLog>> getAllLogsForStudy(
         @PathVariable("logType") String logType,
-        @PathVariable("studyId") String studyId
+        @PathVariable("studyId") String studyId,
+        Authentication authentication
     ) {
-        List<ImportLog> logs = importService.getAllLogsForStudy(studyId, logType);
-        return new ResponseEntity<>(logs, HttpStatus.OK);
+        String userId = getUserId(authentication);
+        HttpGet request = new HttpGet("importer:8080/logs/" + studyId + "/" + logType + "/" + userId);
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            HttpStatus status = HttpStatus.resolve(response.getStatusLine().getStatusCode());
+            ObjectMapper mapper = new ObjectMapper();
+            List<ImportLog> importLogs = Arrays.asList(mapper.readValue(EntityUtils.toString(response.getEntity()), ImportLog[].class));
+            return new ResponseEntity<>(importLogs, status == null ? HttpStatus.INTERNAL_SERVER_ERROR : status);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
     }
 
     @RequestMapping(value = "/importer/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Get a list of all studies in the importer")
-    public ResponseEntity<List<ImportStudy>> getAllImporterStudies() {
-        List<ImportStudy> studies = importService.getAllStudies();
-        return new ResponseEntity<>(studies, HttpStatus.OK);
+    public ResponseEntity<List<ImportStudy>> getAllImporterStudies(Authentication authentication) {
+        String userId = getUserId(authentication);
+        HttpGet request = new HttpGet("importer:8080/studies/" + userId);
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            HttpStatus status = HttpStatus.resolve(response.getStatusLine().getStatusCode());
+            ObjectMapper mapper = new ObjectMapper();
+            List<ImportStudy> studies = Arrays.asList(mapper.readValue(EntityUtils.toString(response.getEntity()), ImportStudy[].class));
+            return new ResponseEntity<>(studies, status == null ? HttpStatus.INTERNAL_SERVER_ERROR : status);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PreAuthorize("hasPermission(#studyId, 'CancerStudyId', 'read')")
     @RequestMapping(value = "/importer/{studyId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Get study details")
     public ResponseEntity<ImportStudy> getImporterStudy(
-        @PathVariable("studyId") String studyId
+        @PathVariable("studyId") String studyId,
+        Authentication authentication
     ) {
-        ImportStudy study = importService.getStudy(studyId);
-        return new ResponseEntity<>(study, HttpStatus.OK);
+        String userId = getUserId(authentication);
+        HttpGet request = new HttpGet("importer:8080/studies/" + studyId + "/" + userId);
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            HttpStatus status = HttpStatus.resolve(response.getStatusLine().getStatusCode());
+            ObjectMapper mapper = new ObjectMapper();
+            ImportStudy importLog = mapper.readValue(EntityUtils.toString(response.getEntity()), ImportStudy.class);
+            return new ResponseEntity<>(importLog, status == null ? HttpStatus.INTERNAL_SERVER_ERROR : status);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PreAuthorize("hasPermission(#studyId, 'CancerStudyId', 'read')")
     @RequestMapping(value = "/importer/{studyId}/import", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Run a trial import of the studyId")
     public ResponseEntity<String> runTrialImport(
         @PathVariable("studyId") String studyId,
         Authentication authentication
     ) throws IOException, InterruptedException {
-        // nb: computeIfAbsent is atomic for ConcurrentHashMap, so this works
-        Semaphore lock = importLocks.computeIfAbsent(studyId, k -> new Semaphore(1, true));
-        lock.acquire();
-        
         String username = getUserName(authentication);
-        ImportStudy study = importService.getStudy(studyId);
-        if (study == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        String userId = getUserId(authentication);
+        HttpGet request = new HttpGet("importer:8080/importer/" + studyId + "/" + username + "/" + userId + "/import");
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            HttpStatus status = HttpStatus.resolve(response.getStatusLine().getStatusCode());
+            return new ResponseEntity<>(status == null ? HttpStatus.INTERNAL_SERVER_ERROR : status);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        if (study.isImportRunning()) {
-            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
-        }
-
-        ImportLog log = new ImportLog();
-        log.setLogType(ImportLogType.Import.name().toLowerCase());
-        log.setTestRun(true);
-        log.setStartDate(new Date());
-        log.setRequester(username);
-        log.setStudyId(studyId);
-        log.setText("<span>Import still running...</span>");
-        study.setImportRunning(true);
-
-        Integer id;
-        importService.addImportLog(log);
-        id = importService.getLastId();
-        importService.updateStudyAsImporting(studyId);
-
-	    String commandTemplate = "%s -command import -path %s -name %s -dbpass %s -dbaddr %s:3306 -username %s -logid %d -log_dir %s -host %s";
-        String command = String.format(commandTemplate, importer, study.getStudyPath(), studyId, dbPassword, dbHost, username, id, logDir, host);
-        String passwordlessCommand = String.format(commandTemplate, importer, study.getStudyPath(), studyId, "PASSWORD", dbHost, username, id, logDir, host);
-        System.out.println(passwordlessCommand);
-        Runtime.getRuntime().exec(command).waitFor();
-        
-        lock.release();
-
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PreAuthorize("hasPermission(#studyId, 'CancerStudyId', 'read')")
     @RequestMapping(value = "/importer/{studyId}/validate", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation("Run a trial validation of the studyId")
     public ResponseEntity<String> runTrialValidation(
         @PathVariable("studyId") String studyId,
         Authentication authentication
     ) throws IOException, InterruptedException {
-        // nb: computeIfAbsent is atomic for ConcurrentHashMap, so this works
-        Semaphore lock = validationLocks.computeIfAbsent(studyId, k -> new Semaphore(1, true));
-        lock.acquire();
-
         String username = getUserName(authentication);
-        ImportStudy study;
+        String userId = getUserId(authentication);
+        HttpGet request = new HttpGet("importer:8080/importer/" + studyId + "/" + username + "/" + userId + "/validate");
 
-        study = importService.getStudy(studyId);
-        if (study == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            HttpStatus status = HttpStatus.resolve(response.getStatusLine().getStatusCode());
+            return new ResponseEntity<>(status == null ? HttpStatus.INTERNAL_SERVER_ERROR : status);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        if (study.isValidationRunning()) {
-            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
-        }
-
-        study.setValidationRunning(true);
-        importService.updateStudyAsValidating(studyId);
-
-	    String commandTemplate = "%s -command validate -path %s -name %s -dbpass %s -dbaddr %s:3306 -username %s -log_dir %s -host %s";
-        String command = String.format(commandTemplate, importer, study.getStudyPath(), studyId, dbPassword, dbHost, username, logDir, host);
-        String passwordlessCommand = String.format(commandTemplate, importer, study.getStudyPath(), studyId, "PASSWORD", dbHost, username, logDir, host);
-
-        System.out.println(passwordlessCommand);
-        Runtime.getRuntime().exec(command);
-        
-        lock.release();
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     private String getUserName(Authentication authentication) {
         String username = authentication == null ? "no_auth" : authentication.getName();
         User user = importService.getUser(username);
         return user == null ? username : user.getName().replace(" ", "_");
+    }
+    
+    private String getUserId(Authentication authentication) {
+        return authentication == null ? "no_auth" : authentication.getName();
     }
 }
